5c94c6e2e23accbc392d49a1402bb3b4ebc6bb44
GitDeno <ddrendy@gmail.com>
2023-08-27 15:12:48 +0200

    Add: email validation on edit by user

--- end_app/accounts/forms.py
+++ end_app/accounts/forms.py
@@ -39,6 +39,15 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ["first_name", "last_name", "email"]
 
+    def clean_email(self):
+        User = get_user_model()
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id)\
+            .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
